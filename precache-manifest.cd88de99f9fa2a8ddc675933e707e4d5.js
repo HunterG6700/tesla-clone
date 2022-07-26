7de836f59dad827b6ae9d7bf9c2c4cbf7d0f851b
Hunter Goller <65684802+HunterG6700@users.noreply.github.com>
2022-07-26 09:20:44 -0400

    Updates

---
+++ precache-manifest.cd88de99f9fa2a8ddc675933e707e4d5.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "be5f3bd2a63e7c61dd41",
+    "url": "/tesla-clone/static/css/main.ce817fa7.chunk.css"
+  },
+  {
+    "revision": "be5f3bd2a63e7c61dd41",
+    "url": "/tesla-clone/static/js/main.be5f3bd2.chunk.js"
+  },
+  {
+    "revision": "7f1662d5cca94d6900a5",
+    "url": "/tesla-clone/static/js/1.7f1662d5.chunk.js"
+  },
+  {
+    "revision": "df6377c38b013df843bf",
+    "url": "/tesla-clone/static/js/2.df6377c3.chunk.js"
+  },
+  {
+    "revision": "e696a512f2c4319ccc14",
+    "url": "/tesla-clone/static/js/runtime~main.e696a512.js"
+  },
+  {
+    "revision": "9ae524bae93935b2622e6d8a4b3a8652",
+    "url": "/tesla-clone/static/media/logo.9ae524ba.svg"
+  },
+  {
+    "revision": "6ad3a7ae55425a97a24ace11fc1c6fb3",
+    "url": "/tesla-clone/static/media/down-arrow.6ad3a7ae.svg"
+  },
+  {
+    "revision": "be6228998e31904c0d42d148597680fa",
+    "url": "/tesla-clone/static/media/logo.be622899.svg"
+  },
+  {
+    "revision": "8f33398725d48caa29849528e441c7cd",
+    "url": "/tesla-clone/static/media/accessories.8f333987.jpg"
+  },
+  {
+    "revision": "03591e64a04d786197adf204def154bb",
+    "url": "/tesla-clone/static/media/audio.03591e64.jpg"
+  },
+  {
+    "revision": "16765dbe2eafb6dbf261e470bf60419b",
+    "url": "/tesla-clone/static/media/connected.16765dbe.jpg"
+  },
+  {
+    "revision": "d7d74002d22750a81c1189d33d202733",
+    "url": "/tesla-clone/static/media/down-arrow1.d7d74002.svg"
+  },
+  {
+    "revision": "c169d188adc64e6ab52c796b56fc8c91",
+    "url": "/tesla-clone/static/media/favicon.c169d188.ico"
+  },
+  {
+    "revision": "df66af098f2a38a3985801d08c30f43e",
+    "url": "/tesla-clone/static/media/game.df66af09.jpg"
+  },
+  {
+    "revision": "36f291354527fdf27988dc737df30142",
+    "url": "/tesla-clone/static/media/model-3.36f29135.jpg"
+  },
+  {
+    "revision": "2c3fab64131f823e9521b4c6d1c37ed4",
+    "url": "/tesla-clone/static/media/model-s.2c3fab64.jpg"
+  },
+  {
+    "revision": "21ac0164331ae75cfe720648ba22d340",
+    "url": "/tesla-clone/static/media/model-x.21ac0164.jpg"
+  },
+  {
+    "revision": "e2b44db24f53e19bfd69dcc61ab1ee66",
+    "url": "/tesla-clone/static/media/model-y.e2b44db2.jpg"
+  },
+  {
+    "revision": "b01f92c5c37b1d648c686411d2f81791",
+    "url": "/tesla-clone/static/media/new-interior.b01f92c5.jpg"
+  },
+  {
+    "revision": "a1e04a413333039f8590cefadb42bdc1",
+    "url": "/tesla-clone/static/media/solar-panel.a1e04a41.jpg"
+  },
+  {
+    "revision": "a1f0b9e154dc630db1e0dce493558550",
+    "url": "/tesla-clone/static/media/solar-roof.a1f0b9e1.jpg"
+  },
+  {
+    "revision": "5a216d3e3bfde9019630400ad5d6ea97",
+    "url": "/tesla-clone/index.html"
+  }
+];
