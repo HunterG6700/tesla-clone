17f05a826a007a824a97bf8e3dacc0a6f1ff9bab
Hunter Goller <65684802+HunterG6700@users.noreply.github.com>
2022-07-25 20:41:21 -0400

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+        <Section 
+            title="Model S"
+            description="Order Online for Touchless Delivery"
+            backgroundImg = "model-s.jpg"
+            lbuttonText="Custom Order"
+            rbuttonText="Existing Inventory"
+        />
+          <Section 
+            title="Model Y"
+            description="Order Online for Touchless Delivery"
+            backgroundImg = 'model-y.jpg'
+            lbuttonText="Custom Order"
+            rbuttonText="Existing Inventory"
+        />
+        <Section 
+            title="Model 3"
+            description="Order Online for Touchless Delivery"
+            backgroundImg = 'model-3.jpg'
+            lbuttonText="Custom Order"
+            rbuttonText="Existing Inventory"
+        />
+           <Section 
+              title="Model X"
+              description="Order Online for Touchless Delivery"
+              backgroundImg = 'model-x.jpg'
+              lbuttonText="Custom Order"
+              rbuttonText="Existing Inventory"
+        />
+        <Section 
+            title="Solar Panels" 
+            description="Lowest Cost Solar Panels in America"
+            backgroundImg = 'solar-panel.jpg'
+            lbuttonText="Order now"
+            rbuttonText="Learn More"
+        />
+        <Section 
+            title="Solar Roof" 
+            description="Produce Clean Energy From Your Roof"
+            backgroundImg = 'solar-roof.jpg'
+            lbuttonText="Order now"
+            rbuttonText="Learn More"
+        />
+        <Section 
+            title="Accessories" 
+            description=""
+            backgroundImg = 'accessories.jpg'
+            lbuttonText="Shop now"
+            rbuttonText=""
+        />
+    
+    
+    
+        
+    </Container>
+  )
+}
+
+export default Home
+const Container = styled.div`
+    height: 100vh;
+
+`
